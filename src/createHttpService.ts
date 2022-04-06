d8a073cfc92ae902a4aa221f3b67b60645a7e0cd
rana.lubis <rana.lubis@gmail.com>
2022-04-06 08:25:34 +0700

    add https options

--- src/createHttpService.ts
+++ src/createHttpService.ts
@@ -1,15 +1,19 @@
 import { RequestListener } from "http";
 import path from "path";
 import http from "http";
+import https from "https";
 import os from "os";
 
-export const createHttpService = (handler: RequestListener) => {
+export declare type IHttpsService = {
+  key: any;
+  cert: any;
+};
+
+const createService = (server: http.Server) => {
   const PATH = path.join(
     os.platform() === "win32" ? "\\\\?\\pipe" : "/tmp",
     process.argv[2] || "default"
   );
-
-  const server = http.createServer(handler);
 
   if (process.send || process.env.CHILD) {
     process.send({ type: "http init", socketPath: PATH });
@@ -21,4 +25,19 @@ export const createHttpService = (handler: RequestListener) => {
 
     return server;
   };
+};
+
+export const createHttpService = (handler: RequestListener) => {
+  const server = http.createServer(handler);
+
+  return createService(server);
+};
+
+export const createHttpsService = (
+  options: IHttpsService,
+  handler: RequestListener
+) => {
+  const server = https.createServer(options, handler);
+
+  return createService(server);
 };
